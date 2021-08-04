53c68ff7229809468803c4700cd8d525bcc58ee0
Sercan Yersen <sercan@jotform.com>
2021-08-04 18:27:04 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component, useEffect, useState } from 'react';
 
-function App() {
+// // state
+// class Example {
+//   constructor() {
+
+//   }
+
+//   get prop() {
+
+//   }
+
+//   method() {
+
+//   }
+// }
+
+// class ExampleComponent extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       buttonClicked: false,
+//     };
+
+//     // this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   // life cycle method
+//   componentDidMount() {
+//     document.addEventListener('click', this.amazingEventListener);
+//   }
+
+//   componentWillUnmount() {
+//     document.removeEventListener('click', this.amazingEventListener);
+//   }
+
+//   componentDidUpdate() {
+
+//   }
+
+//   shouldComponentUpdate() {
+//     return true;
+//   }
+
+//   // state = {
+//   //   buttonClicked: false,
+//   // }
+
+//   handleClick = (event) => {
+//     this.setState({
+//       buttonClicked: true,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             {this.props.selam}
+//           </p>
+//           <button type="button" onClick={this.handleClick}>
+//             Click me
+//           </button>
+//           <div>
+//             {this.state.buttonClicked ? 'Clicked' : 'Not Clicked'}
+//           </div>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+
+
+const FunctionalState = props => {
+  // hooks
+  // useState
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  function handleClick() {
+    setButtonClicked(!buttonClicked);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {props.selam}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button" onClick={handleClick}>
+          Click me
+        </button>
+        <div>
+          {buttonClicked ? 'Clicked' : 'Not Clicked'}
+        </div>
       </header>
     </div>
   );
-}
+};
 
-export default App;
+const ComponentRenderer = props => {
+  
+};
+
+export default ComponentRenderer;
+
+// functional
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+const util = () => console.log('default export');
+
+export const named = () => console.log('named export');
+
+export default util;
